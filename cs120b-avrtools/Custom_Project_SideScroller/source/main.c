f4a84ce8afe0639fcab62addc3ca3569326991fc
Alexander Ku <aku006@wch136-03.cs.ucr.edu>
2019-07-21 21:52:45 -0700

    Saving progress

--- cs120b-avrtools/Custom_Project_SideScroller/source/main.c
+++ cs120b-avrtools/Custom_Project_SideScroller/source/main.c
@@ -16,9 +16,17 @@
 #include "../header/scheduler.h"
 #endif
 
-#define input (~PINC & 0x0F);
+#define input (~PINB & 0x0F)
+
+typedef struct _player {
+	unsigned char x;
+	unsigned char y;
+} player;
+
+player character[7];
 
 /* Example code */
+/*
 enum nokia_States {nok_init, nok_display};
 
 int displaySMTick(int state) {
@@ -48,23 +56,137 @@ int displaySMTick(int state) {
 			break;
 	}
 	return state;
-}
+}*/
 
+/* Player movement*/
+
+enum player_States { p_init, p_wait, p_press, p_up, p_down };
+
+int playerSMTick(int state) {
+	unsigned char i = 0;
+	//unsigned char j = 0;
+	switch(state) {
+		case p_init:
+			break;
+		case p_wait:
+			if (input == 0x01 || input == 0x02) {
+				state = p_up;
+			}
+			else {
+				state = p_wait;
+			}
+			state = p_wait;
+			break;
+		case p_press:
+			if (input == 0x01 && input != 0x02) {
+				state = p_up;
+			}
+			else if (input != 0x01 && input == 0x02) {
+				state = p_down;
+			}
+			break;
+		case p_up:
+			state = p_wait;
+			break;
+		case p_down:
+			state = p_wait;
+			break;
+		default:
+			state = p_init;
+			break;
+	}
+	switch(state) {
+		case p_init:
+			//nokia_lcd_init();
+			for (i = 0; i < 7; i++) {
+				nokia_lcd_set_pixel(character[i].x, character[i].y, 1);
+			}
+			//nokia_lcd_clear();
+			nokia_lcd_render();
+			state = p_wait;
+			break;
+		case p_wait:
+			break;
+		case p_press:
+			break;
+		case p_up:
+			if (character[0].y == 1) {
+				for (i = 0; i < 7; i++) {
+					nokia_lcd_set_pixel(character[i].x, character[i].y, 1);
+				}
+			}
+			else {
+				//for (i = 0; i < 7; i++) {
+				//	nokia_lcd_set_pixel(character[i].x, character[i].y, 0);
+			//	}
+				nokia_lcd_clear();
+				for (i = 0; i < 7; i++) {
+					//character[i].y = character[i].y - 1;
+					nokia_lcd_set_pixel(character[i].x, character[i].y - 1, 1);
+				}
+			}
+			nokia_lcd_render();
+			//state = p_wait;
+			break;
+		case p_down:
+			if (character[7].y == 42) {
+				for (i = 0; i < 7; i++) {
+					nokia_lcd_set_pixel(character[i].x, character[i].y, 1);
+				}
+			}
+			else {
+//				for (i = 0; i < 7; i++) {
+//					nokia_lcd_set_pixel(character[i].x, character[i].y, 0);
+//				}
+				nokia_lcd_clear();
+				for (i = 0; i < 7; i++) {
+					//character[i].y = character[i].y + 1;
+					nokia_lcd_set_pixel(character[i].x, character[i].y + 1, 1);
+				}
+			}
+			nokia_lcd_render();
+			//state = p_wait;
+			break;
+		default:
+			break;
+	}
+}
+			
 int main(void) {
 	/* Insert DDR and PORT initializations */
-	DDRC = 0x00; PORTC = 0xFF;
+	DDRA = 0xFF; PORTA = 0x00;
+	DDRB = 0x00; PORTB = 0xFF;
 
 	/* Insert your solution below */
-	static task screen_Task;
-	task *tasks[] = { &screen_Task };
+	//player character[7];
+
+	character[0].x = 1;
+	character[1].x = 2;
+	character[2].x = 1;
+	character[3].x = 2;
+	character[4].x = 3;
+	character[5].x = 2;
+	character[6].x = 1;
+
+	character[0].y = 1;
+	character[1].y = 2;
+	character[2].y = 3;
+	character[3].y = 3;
+	character[4].y = 3;
+	character[5].y = 4;
+	character[6].y = 5;
+
+	static task player_Task;
+	task *tasks[] = { &player_Task };
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
 	/*Tasks*/
-	screen_Task.state = nok_init;
-	screen_Task.period = 5;
-	screen_Task.elapsedTime = screen_Task.period;
-	screen_Task.TickFct = &displaySMTick;
+	player_Task.state = p_init;
+	player_Task.period = 1;
+	player_Task.elapsedTime = 1;
+	player_Task.TickFct = &playerSMTick;
 
+	nokia_lcd_init();
 	TimerSet(1);
 	TimerOn();
 
@@ -81,5 +203,5 @@ int main(void) {
 		TimerFlag = 0;
 		_delay_ms(1000);
 	}
-	return 0;
+	return 1;
 }
